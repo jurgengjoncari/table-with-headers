58a7c3e9f2128828624510905b0a633da08a4268
Jurgen Gjonçari <j.gjoncari@albansi.com>
2022-02-17 23:41:10 +0100

    First and probably last

---
+++ timetable.js
@@ -0,0 +1,8 @@
+export const table = [
+    ["", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat", "Sun"],
+    ["1st period", "English", "", "", "German", "Dutch", "", ""],
+    ["2nd period", "English", "English", "", "German", "Dutch", "", ""],
+    ["3rd period", "", "German", "", "German", "Dutch", "", ""],
+    ["4th period", "", "English", "", "English", "Dutch", "", ""]
+]
+ 
